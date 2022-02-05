ab9abf1049dde8a33ce02dfcadea904a85e81501
poojachoudhary560 <poojachoudhary560@gmail.com>
2022-02-06 03:31:30 +0530

    1 file modified

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import fetchData from '../../api/service';
 
 import './index.css';
@@ -7,7 +7,7 @@ const SearchBox = (props) => {
   const [searchKey, setSearchKey] = useState('');
   const [displaySearch, setDisplaySearch] = useState(false);
   const [searchResult, setSearchResult] = useState([]);
-
+  const myRef = useRef();
   useEffect(() => {
     if (searchKey) {
       fetchData(searchKey).then((res) => {
@@ -21,13 +21,29 @@ const SearchBox = (props) => {
     setSearchKey(el.item);
     setDisplaySearch(false);
   };
+
+  const handleBlur = (event) => {
+    // currentTarget refers to this component.
+    // relatedTarget refers to the element where the user clicked (or focused) which
+    // triggered this event.
+    // So in effect, this condition checks if the user clicked outside the component.
+    console.log(event.currentTarget, event.relatedTarget);
+    /* if (!event.currentTarget.contains(event.relatedTarget)) {
+      // do your thing.
+      setDisplaySearch(false);
+    } */
+    if (!myRef.current.contains(event.target)) {
+      setDisplaySearch(false);
+    }
+  };
   return (
     <>
-      <div className="autocomplete">
+      <div className="autocomplete" ref={myRef}>
         <input
           type="text"
           id="search"
           onClick={() => setDisplaySearch(true)}
+          onBlur={handleBlur}
           value={searchKey}
           onChange={(e) => setSearchKey(e.target.value)}
         />
