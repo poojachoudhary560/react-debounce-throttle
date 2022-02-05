95ffe2f419aeec44d2ccb4491ef5da0828e95ad9
poojachoudhary560 <poojachoudhary560@gmail.com>
2022-02-06 03:14:20 +0530

    1 file modified

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import fetchData from '../../api/service';
+
 import './index.css';
 
 const SearchBox = (props) => {
   const [searchKey, setSearchKey] = useState('');
   const [displaySearch, setDisplaySearch] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
 
+  useEffect(() => {
+    if (searchKey) {
+      fetchData(searchKey).then((res) => {
+        setSearchResult(res);
+      });
+    }
+  }, [searchKey]);
+
+  const handleElementSelect = (el) => {
+    console.log(el);
+    setSearchKey(el.item);
+    setDisplaySearch(false);
+  };
   return (
     <>
       <div className="autocomplete">
@@ -13,18 +29,19 @@ const SearchBox = (props) => {
           id="search"
           onClick={() => setDisplaySearch(true)}
           value={searchKey}
+          onChange={(e) => setSearchKey(e.target.value)}
         />
         <div
           className={`autocomplete-items ${
             displaySearch ? '' : 'autocomplete-items-hide'
           }`}
         >
-          <div>India</div>
-          <div>India</div>
-          <div>India</div>
-          <div>India</div>
+          {searchResult.map((el) => (
+            <div onClick={() => handleElementSelect(el)}>{el.item}</div>
+          ))}
         </div>
       </div>
+      {JSON.stringify(searchResult)}
     </>
   );
 };
